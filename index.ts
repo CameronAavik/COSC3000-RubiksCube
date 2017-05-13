1afb42e1b3c7d4df0a6bd151a4694afd81848bdd
Cameron Aavik <cameronaavik@gmail.com>
2017-05-14 00:57:00 +1000

    Change to a rectangle Change to use triangle strip Add starting work on getting rubiks cubr data structure

--- index.ts
+++ index.ts
@@ -1,3 +1,196 @@
+type CubieFace = { cubie: Cubie, colour: Num4 };
+enum Entrance { Top, Bottom, Left, Right };
+enum Face { Front, Back, Left, Right, Up, Down };
+type FaceEntrance = { face: RubiksFace, entrance: Entrance };
+type Num4 = [number, number, number, number];
+
+class Colour {
+    public static readonly White: Num4 = [1, 1, 1, 1];
+    public static readonly Blue: Num4 = [0, 0x51 / 0xFF, 0xBA / 0xFF, 1];
+    public static readonly Yellow: Num4 = [1, 0xD5 / 0xFF, 0, 1];
+    public static readonly Green: Num4 = [0, 0x9E / 0xFF, 0x60 / 0xFF, 1];
+    public static readonly Red: Num4 = [0xC4 / 0xFF, 0x1E / 0xFF, 0x3A / 0xFF, 1];
+    public static readonly Orange: Num4 = [1, 0x58 / 0xFF, 0, 1];
+}
+
+abstract class Cubie { }
+class CornerCubie extends Cubie { }
+class EdgeCubie extends Cubie { }
+class CenterCubie extends Cubie { }
+
+class RubiksFace {
+    public up: FaceEntrance;
+    public down: FaceEntrance;
+    public left: FaceEntrance;
+    public right: FaceEntrance;
+    constructor(public cubies: CubieFace[][]) { }
+}
+
+type RubiksCubeFaces = {
+    front: RubiksFace, back: RubiksFace,
+    left: RubiksFace, right: RubiksFace,
+    up: RubiksFace, down: RubiksFace,
+};
+
+class RubiksCube {
+    public cubies: Cubie[];
+
+    public faces: RubiksCubeFaces
+
+    constructor(n: number) {
+        this.cubies = RubiksCube.genCubies(n);
+        this.faces = {
+            front: RubiksCube.genFace(n, this.cubies, Face.Front),
+            back: RubiksCube.genFace(n, this.cubies, Face.Back),
+            left: RubiksCube.genFace(n, this.cubies, Face.Left),
+            right: RubiksCube.genFace(n, this.cubies, Face.Right),
+            up: RubiksCube.genFace(n, this.cubies, Face.Up),
+            down: RubiksCube.genFace(n, this.cubies, Face.Down),
+        }
+        RubiksCube.genFaceEntrances(this.faces);
+    }
+
+    private static genCubies(n: number): Cubie[] {
+        const cubies = new Array() as Cubie[];
+        // corners
+        for (let i = 0; i < 8; i++) {
+            cubies.push(new CornerCubie());
+        }
+        // edges
+        for (let i = 0; i < 12 * (n - 2); i++) {
+            cubies.push(new EdgeCubie());
+        }
+        // centers
+        for (let i = 0; i < 6 * (n - 2) ** 2; i++) {
+            cubies.push(new CenterCubie());
+        }
+        return cubies;
+    }
+
+    private static genFace(n: number, cubies: Cubie[], face: Face): RubiksFace {
+        const corners = RubiksCube.getCornerIndices(face);
+        const edges = RubiksCube.getEdgeStartIndices(face);
+        const centers = RubiksCube.getCenterStartIndices(face);
+        const colour = RubiksCube.getColourForFace(face);
+        const cubieMatrix = [] as CubieFace[][];
+        for (let row = 0; row < n; row++) {
+            cubieMatrix[row] = [] as CubieFace[];
+            for (let col = 0; col < n; col++) {
+                let index = 0; // Will get overriden
+                if (row === 0) {
+                    if (col === 0) {
+                        index = corners[0];
+                    } else if (col === n - 1) {
+                        index = corners[1];
+                    } else {
+                        index = 8 + edges[0] * (n - 2);
+                    }
+                } else if (row === n - 1) {
+                    if (col === 0) {
+                        index = corners[2];
+                    } else if (col === n - 1) {
+                        index = corners[3];
+                    } else {
+                        index = 8 + edges[3] * (n - 2);
+                    }
+                } else {
+                    if (col === 0) {
+                        index = 8 + edges[1] * (n - 2) + n - (row + 2);
+                    } else if (col === n - 1) {
+                        index = 8 + edges[2] * (n - 2) + row - 1;
+                    } else {
+                        index = 8 + 12 * (n - 2) + centers * (n - 2) ** 2;
+                    }
+                }
+                cubieMatrix[row].push({ cubie: cubies[index], colour });
+            }
+        }
+        return new RubiksFace(cubieMatrix);
+    }
+
+    private static genFaceEntrances(faces: RubiksCubeFaces): void {
+        // not sure of a better way to do this :/
+        faces.front.up = {face: faces.up, entrance: Entrance.Bottom};
+        faces.front.down = {face: faces.down, entrance: Entrance.Top};
+        faces.front.left = {face: faces.left, entrance: Entrance.Right};
+        faces.front.right = {face: faces.right, entrance: Entrance.Left};
+
+        faces.back.up = {face: faces.up, entrance: Entrance.Top};
+        faces.back.down = {face: faces.down, entrance: Entrance.Bottom};
+        faces.back.left = {face: faces.right, entrance: Entrance.Right};
+        faces.back.right = {face: faces.left, entrance: Entrance.Left};
+
+        faces.left.up = {face: faces.up, entrance: Entrance.Left};
+        faces.left.down = {face: faces.down, entrance: Entrance.Left};
+        faces.left.left = {face: faces.back, entrance: Entrance.Right};
+        faces.left.right = {face: faces.front, entrance: Entrance.Left};
+
+        faces.right.up = {face: faces.up, entrance: Entrance.Right};
+        faces.right.down = {face: faces.down, entrance: Entrance.Right};
+        faces.right.left = {face: faces.front, entrance: Entrance.Right};
+        faces.right.right = {face: faces.back, entrance: Entrance.Left};
+
+        faces.up.up = {face: faces.back, entrance: Entrance.Top};
+        faces.up.down = {face: faces.front, entrance: Entrance.Top};
+        faces.up.left = {face: faces.left, entrance: Entrance.Top};
+        faces.up.right = {face: faces.right, entrance: Entrance.Top};
+
+        faces.down.up = {face: faces.front, entrance: Entrance.Bottom};
+        faces.down.down = {face: faces.back, entrance: Entrance.Bottom};
+        faces.down.left = {face: faces.left, entrance: Entrance.Bottom};
+        faces.down.right = {face: faces.right, entrance: Entrance.Bottom};
+    }
+
+    // Gets which corners are part of the given face (in order of top-left, top-right, bottom-left, bottom-right)
+    private static getCornerIndices(face: Face): Num4 {
+        switch (face) {
+            case Face.Front: return [0, 1, 2, 3];
+            case Face.Back: return [4, 5, 6, 7];
+            case Face.Left: return [5, 0, 7, 2];
+            case Face.Right: return [1, 4, 3, 6];
+            case Face.Up: return [5, 4, 0, 1];
+            case Face.Down: return [2, 3, 7, 6];
+        }
+    }
+
+
+    // Gets relative index of each edge (in order of top, left, right, down)
+    private static getEdgeStartIndices(face: Face): Num4 {
+        switch (face) {
+            case Face.Front: return [0, 1, 2, 3];
+            case Face.Back: return [4, 5, 6, 7];
+            case Face.Left: return [8, 6, 9, 1];
+            case Face.Right: return [10, 2, 5, 11];
+            case Face.Up: return [4, 8, 10, 0];
+            case Face.Down: return [3, 9, 11, 7];
+        }
+    }
+
+    // Gets relative index of center cubi
+    private static getCenterStartIndices(face: Face): number {
+        switch (face) {
+            case Face.Front: return 0;
+            case Face.Back: return 1;
+            case Face.Left: return 2;
+            case Face.Right: return 3;
+            case Face.Up: return 4;
+            case Face.Down: return 5;
+        }
+    }
+
+    // Gets default colour for each face
+    private static getColourForFace(face: Face): Num4 {
+        switch (face) {
+            case Face.Front: return Colour.Yellow;
+            case Face.Back: return Colour.White;
+            case Face.Left: return Colour.Orange;
+            case Face.Right: return Colour.Red;
+            case Face.Up: return Colour.Blue;
+            case Face.Down: return Colour.Green;
+        }
+    }
+}
+
 class WebGLDemo {
 
     constructor(private canvasId: string, private vertexShaderPath: string, private fragmentShaderPath: string) { }
@@ -16,11 +209,12 @@ class WebGLDemo {
         const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
         const positionBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-        // three 2d points
+        // four 3d points (square)
         const positions = [
-            -0.5, -0.5,
-            0.5, -0.5,
-            0, 0.5,
+            0.5, 0.5, 0.0,
+            -0.5, 0.5, 0.0,
+            0.5, -0.5, 0.0,
+            -0.5, -0.5, 0.0
         ];
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
@@ -39,17 +233,13 @@ class WebGLDemo {
         gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
         // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-        const size = 2;          // 2 components per iteration
+        const size = 3;          // 2 components per iteration
         const type = gl.FLOAT;   // the data is 32bit floats
         const normalize = false; // don't normalize the data
         const stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
         const bufferOffset = 0;  // start at the beginning of the buffer
         gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, bufferOffset);
-
-        const primitiveType = gl.TRIANGLES;
-        const offset = 0;
-        const count = 3;
-        gl.drawArrays(primitiveType, offset, count);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
     }
 
     private static loadGLContext(canvasId: string): Promise<WebGLRenderingContext> {
@@ -85,7 +275,7 @@ class WebGLDemo {
         return Promise.all([
             WebGLDemo.createShader(gl, vertexShader, gl.VERTEX_SHADER),
             WebGLDemo.createShader(gl, fragmentShader, gl.FRAGMENT_SHADER)
-        ]).then(([vertexShader, fragmentShader]) => ({ vertexShader, fragmentShader}));
+        ]).then(([vertexShader, fragmentShader]) => ({ vertexShader, fragmentShader }));
     }
 
     private static createShader(gl: WebGLRenderingContext, source: string, type: number): Promise<WebGLShader> {
--- js/index.js
+++ js/index.js
@@ -1,4 +1,192 @@
 "use strict";
+var Entrance;
+(function (Entrance) {
+    Entrance[Entrance["Top"] = 0] = "Top";
+    Entrance[Entrance["Bottom"] = 1] = "Bottom";
+    Entrance[Entrance["Left"] = 2] = "Left";
+    Entrance[Entrance["Right"] = 3] = "Right";
+})(Entrance || (Entrance = {}));
+;
+var Face;
+(function (Face) {
+    Face[Face["Front"] = 0] = "Front";
+    Face[Face["Back"] = 1] = "Back";
+    Face[Face["Left"] = 2] = "Left";
+    Face[Face["Right"] = 3] = "Right";
+    Face[Face["Up"] = 4] = "Up";
+    Face[Face["Down"] = 5] = "Down";
+})(Face || (Face = {}));
+;
+class Colour {
+}
+Colour.White = [1, 1, 1, 1];
+Colour.Blue = [0, 0x51 / 0xFF, 0xBA / 0xFF, 1];
+Colour.Yellow = [1, 0xD5 / 0xFF, 0, 1];
+Colour.Green = [0, 0x9E / 0xFF, 0x60 / 0xFF, 1];
+Colour.Red = [0xC4 / 0xFF, 0x1E / 0xFF, 0x3A / 0xFF, 1];
+Colour.Orange = [1, 0x58 / 0xFF, 0, 1];
+class Cubie {
+}
+class CornerCubie extends Cubie {
+}
+class EdgeCubie extends Cubie {
+}
+class CenterCubie extends Cubie {
+}
+class RubiksFace {
+    constructor(cubies) {
+        this.cubies = cubies;
+    }
+}
+class RubiksCube {
+    constructor(n) {
+        this.cubies = RubiksCube.genCubies(n);
+        this.faces = {
+            front: RubiksCube.genFace(n, this.cubies, Face.Front),
+            back: RubiksCube.genFace(n, this.cubies, Face.Back),
+            left: RubiksCube.genFace(n, this.cubies, Face.Left),
+            right: RubiksCube.genFace(n, this.cubies, Face.Right),
+            up: RubiksCube.genFace(n, this.cubies, Face.Up),
+            down: RubiksCube.genFace(n, this.cubies, Face.Down),
+        };
+        RubiksCube.genFaceEntrances(this.faces);
+    }
+    static genCubies(n) {
+        const cubies = new Array();
+        // corners
+        for (let i = 0; i < 8; i++) {
+            cubies.push(new CornerCubie());
+        }
+        // edges
+        for (let i = 0; i < 12 * (n - 2); i++) {
+            cubies.push(new EdgeCubie());
+        }
+        // centers
+        for (let i = 0; i < 6 * Math.pow((n - 2), 2); i++) {
+            cubies.push(new CenterCubie());
+        }
+        return cubies;
+    }
+    static genFace(n, cubies, face) {
+        const corners = RubiksCube.getCornerIndices(face);
+        const edges = RubiksCube.getEdgeStartIndices(face);
+        const centers = RubiksCube.getCenterStartIndices(face);
+        const colour = RubiksCube.getColourForFace(face);
+        const cubieMatrix = [];
+        for (let row = 0; row < n; row++) {
+            cubieMatrix[row] = [];
+            for (let col = 0; col < n; col++) {
+                let index = 0; // Will get overriden
+                if (row === 0) {
+                    if (col === 0) {
+                        index = corners[0];
+                    }
+                    else if (col === n - 1) {
+                        index = corners[1];
+                    }
+                    else {
+                        index = 8 + edges[0] * (n - 2);
+                    }
+                }
+                else if (row === n - 1) {
+                    if (col === 0) {
+                        index = corners[2];
+                    }
+                    else if (col === n - 1) {
+                        index = corners[3];
+                    }
+                    else {
+                        index = 8 + edges[3] * (n - 2);
+                    }
+                }
+                else {
+                    if (col === 0) {
+                        index = 8 + edges[1] * (n - 2) + n - (row + 2);
+                    }
+                    else if (col === n - 1) {
+                        index = 8 + edges[2] * (n - 2) + row - 1;
+                    }
+                    else {
+                        index = 8 + 12 * (n - 2) + centers * Math.pow((n - 2), 2);
+                    }
+                }
+                cubieMatrix[row].push({ cubie: cubies[index], colour });
+            }
+        }
+        return new RubiksFace(cubieMatrix);
+    }
+    static genFaceEntrances(faces) {
+        // not sure of a better way to do this :/
+        faces.front.up = { face: faces.up, entrance: Entrance.Bottom };
+        faces.front.down = { face: faces.down, entrance: Entrance.Top };
+        faces.front.left = { face: faces.left, entrance: Entrance.Right };
+        faces.front.right = { face: faces.right, entrance: Entrance.Left };
+        faces.back.up = { face: faces.up, entrance: Entrance.Top };
+        faces.back.down = { face: faces.down, entrance: Entrance.Bottom };
+        faces.back.left = { face: faces.right, entrance: Entrance.Right };
+        faces.back.right = { face: faces.left, entrance: Entrance.Left };
+        faces.left.up = { face: faces.up, entrance: Entrance.Left };
+        faces.left.down = { face: faces.down, entrance: Entrance.Left };
+        faces.left.left = { face: faces.back, entrance: Entrance.Right };
+        faces.left.right = { face: faces.front, entrance: Entrance.Left };
+        faces.right.up = { face: faces.up, entrance: Entrance.Right };
+        faces.right.down = { face: faces.down, entrance: Entrance.Right };
+        faces.right.left = { face: faces.front, entrance: Entrance.Right };
+        faces.right.right = { face: faces.back, entrance: Entrance.Left };
+        faces.up.up = { face: faces.back, entrance: Entrance.Top };
+        faces.up.down = { face: faces.front, entrance: Entrance.Top };
+        faces.up.left = { face: faces.left, entrance: Entrance.Top };
+        faces.up.right = { face: faces.right, entrance: Entrance.Top };
+        faces.down.up = { face: faces.front, entrance: Entrance.Bottom };
+        faces.down.down = { face: faces.back, entrance: Entrance.Bottom };
+        faces.down.left = { face: faces.left, entrance: Entrance.Bottom };
+        faces.down.right = { face: faces.right, entrance: Entrance.Bottom };
+    }
+    // Gets which corners are part of the given face (in order of top-left, top-right, bottom-left, bottom-right)
+    static getCornerIndices(face) {
+        switch (face) {
+            case Face.Front: return [0, 1, 2, 3];
+            case Face.Back: return [4, 5, 6, 7];
+            case Face.Left: return [5, 0, 7, 2];
+            case Face.Right: return [1, 4, 3, 6];
+            case Face.Up: return [5, 4, 0, 1];
+            case Face.Down: return [2, 3, 7, 6];
+        }
+    }
+    // Gets relative index of each edge (in order of top, left, right, down)
+    static getEdgeStartIndices(face) {
+        switch (face) {
+            case Face.Front: return [0, 1, 2, 3];
+            case Face.Back: return [4, 5, 6, 7];
+            case Face.Left: return [8, 6, 9, 1];
+            case Face.Right: return [10, 2, 5, 11];
+            case Face.Up: return [4, 8, 10, 0];
+            case Face.Down: return [3, 9, 11, 7];
+        }
+    }
+    // Gets relative index of center cubi
+    static getCenterStartIndices(face) {
+        switch (face) {
+            case Face.Front: return 0;
+            case Face.Back: return 1;
+            case Face.Left: return 2;
+            case Face.Right: return 3;
+            case Face.Up: return 4;
+            case Face.Down: return 5;
+        }
+    }
+    // Gets default colour for each face
+    static getColourForFace(face) {
+        switch (face) {
+            case Face.Front: return Colour.Yellow;
+            case Face.Back: return Colour.White;
+            case Face.Left: return Colour.Orange;
+            case Face.Right: return Colour.Red;
+            case Face.Up: return Colour.Blue;
+            case Face.Down: return Colour.Green;
+        }
+    }
+}
 class WebGLDemo {
     constructor(canvasId, vertexShaderPath, fragmentShaderPath) {
         this.canvasId = canvasId;
@@ -18,11 +206,12 @@ class WebGLDemo {
         const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
         const positionBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-        // three 2d points
+        // four 3d points (square)
         const positions = [
-            -0.5, -0.5,
-            0.5, -0.5,
-            0, 0.5,
+            0.5, 0.5, 0.0,
+            -0.5, 0.5, 0.0,
+            0.5, -0.5, 0.0,
+            -0.5, -0.5, 0.0
         ];
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
@@ -40,16 +229,13 @@ class WebGLDemo {
         // Bind the position buffer.
         gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
         // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-        const size = 2; // 2 components per iteration
+        const size = 3; // 2 components per iteration
         const type = gl.FLOAT; // the data is 32bit floats
         const normalize = false; // don't normalize the data
         const stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
         const bufferOffset = 0; // start at the beginning of the buffer
         gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, bufferOffset);
-        const primitiveType = gl.TRIANGLES;
-        const offset = 0;
-        const count = 3;
-        gl.drawArrays(primitiveType, offset, count);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
     }
     static loadGLContext(canvasId) {
         return new Promise((resolve, reject) => {
