c5ed1a153885251fd7ec6830df2ba1dcf00518ce
Cameron Aavik <cameronaavik@gmail.com>
2017-05-23 18:30:33 +1000

    Re-did the rubik's class again

--- js/rubik.js
+++ js/rubik.js
@@ -1,47 +1,60 @@
 "use strict";
 var Rubik;
 (function (Rubik) {
-    const IdentityMatrix = [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
-    /**
-     * Multiplies two matrices and returns the result
-     * @param a A 3x3 Matrix
-     * @param b A 3x3 Matrix
-     */
-    function multiplyMatrix(a, b) {
-        return [0, 1, 2].map(i => [0, 1, 2].map(j => [0, 1, 2].map(k => a[i][k] * b[k][j]).reduce((a, b) => a + b, 0)));
-    }
-    /**
-     * See https://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
-     * @param axis The axis of rotation
-     * @param angle The angle being rotated in radians (use right handed rule)
-     */
-    function getRotationMatrix(axis, angle) {
-        const [x, y, z] = axis;
-        const cos = Math.cos(angle);
-        const mcos = 1 - cos;
-        const sin = Math.sin(angle);
-        return [
-            [cos + x * x * mcos, x * y * mcos - x * sin, x * z * mcos + y * sin],
-            [y * x * mcos + z * sin, cos + y * y * mcos, y * z * mcos - x * sin],
-            [z * x * mcos - y * sin, z * y * mcos + x * sin, cos + z * z * mcos]
-        ];
-    }
     /**
      * Creates a new Cube of a given size
      * @param size The size of the cube. a 3x3 cube is size 3
      */
     function createCube(size) {
         const cubies = [];
-        for (let z = 0; z < size; z++) {
-            for (let y = 0; y < size; y++) {
-                for (let x = 0; x < size; x++) {
-                    cubies.push({ startPos: [x, y, z], rotationMatrix: IdentityMatrix });
-                }
-            }
-        }
+        range(0, size).forEach(z => {
+            range(0, size).forEach(y => {
+                range(0, size).forEach(x => {
+                    cubies.push({ startPos: [x, y, z], faces: [0, 1, 2, 3, 4, 5] });
+                });
+            });
+        });
         return { cubies, size };
     }
     Rubik.createCube = createCube;
+    /**
+    * Rotates a given layer of the cube and returns the new cube
+    * @param cube The cube which is getting it's layer rotated
+    * @param rotationLayer The information about the layer being rotated
+    */
+    function rotateLayer(cube, rotationLayer) {
+        const getCubieIndex = (i, j) => getCubieIndexInLayerPos(cube.size, i, j, rotationLayer);
+        const cubies = cube.cubies.slice(); // Create a copy of the cube
+        range(0, cube.size).forEach(i => {
+            range(0, cube.size).forEach(j => {
+                const oldCubie = cube.cubies[getCubieIndex(cube.size - j - 1, i)];
+                const newFaceMap = getFacesAfterRotation(oldCubie.faces, rotationLayer.axis);
+                cubies[getCubieIndex(i, j)] = { startPos: oldCubie.startPos, faces: newFaceMap };
+            });
+        });
+        return { cubies, size: cube.size };
+    }
+    Rubik.rotateLayer = rotateLayer;
+    /**
+     * Returns the FaceMap after a rotation on a given axis
+     * @param faces A FaceMap which is being rotated
+     * @param axis The axis of rotation
+     */
+    function getFacesAfterRotation(faces, axis) {
+        let cycle;
+        switch (axis) {
+            case 0:
+                cycle = [0, 1, 4, 5, 2, 3];
+                break;
+            case 1:
+                cycle = [5, 4, 2, 3, 0, 1];
+                break;
+            case 2:
+                cycle = [2, 3, 1, 0, 4, 5];
+                break;
+        }
+        return faces.map(f => faces[cycle[f]]);
+    }
     /**
      * Will return the index into the cubies array belonging to the (i, j)th cubie in the layer
      * @param cubeSize The size of the cube
@@ -51,28 +64,10 @@ var Rubik;
      */
     function getCubieIndexInLayerPos(cubeSize, i, j, layer) {
         const pos = [i, j];
-        pos.splice(layer.axisNumber, 0, layer.layerNum);
+        pos.splice(layer.axis, 0, layer.layerNum);
         return pos[0] + cubeSize * pos[1] + (Math.pow(cubeSize, 2)) * pos[2];
     }
-    /**
-     * Rotates a given layer of the cube and returns the new cube
-     * @param cube The cube which is getting it's layer rotated
-     * @param rotationLayer The information about the layer being rotated
-     */
-    function rotateLayer(cube, rotationLayer) {
-        const axis = IdentityMatrix[rotationLayer.axisNumber];
-        const rotationMatrix = getRotationMatrix(axis, Math.PI / 2);
-        const getCubieIndex = (i, j) => getCubieIndexInLayerPos(cube.size, i, j, rotationLayer);
-        const cubies = cube.cubies.slice(); // Create a copy of the cube
-        for (let i = 0; i < cube.size; i++) {
-            for (let j = 0; j < cube.size; j++) {
-                const oldCubie = cube.cubies[getCubieIndex(cube.size - j - 1, i)];
-                const newCubieRotMatrix = multiplyMatrix(oldCubie.rotationMatrix, rotationMatrix);
-                cubies[getCubieIndex(i, j)] = { startPos: oldCubie.startPos, rotationMatrix: newCubieRotMatrix };
-            }
-        }
-        return { cubies, size: cube.size };
-    }
-    Rubik.rotateLayer = rotateLayer;
+    // Helper function for generation the interval [0, max)
+    const range = (max) => Array.from({ length: max }, (_, k) => k);
 })(Rubik || (Rubik = {}));
 //# sourceMappingURL=rubik.js.map
--- rubik.ts
+++ rubik.ts
@@ -1,51 +1,23 @@
 namespace Rubik {
-    // Very simple definition of a vector and matrix for the purposes of the cube
-    type Vec3 = [number, number, number] & { 3?: void };
-    type Matrix3 = [Vec3, Vec3, Vec3] & { 3?: void };
-    const IdentityMatrix: Matrix3 = [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
-
-    /**
-     * Multiplies two matrices and returns the result
-     * @param a A 3x3 Matrix
-     * @param b A 3x3 Matrix
-     */
-    function multiplyMatrix(a: Matrix3, b: Matrix3): Matrix3 {
-        return [0, 1, 2].map(
-            i => [0, 1, 2].map(
-                j => [0, 1, 2].map(k => a[i][k] * b[k][j]).reduce((a, b) => a + b, 0)
-            )
-        ) as Matrix3;
-    }
-
-    /**
-     * See https://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
-     * @param axis The axis of rotation
-     * @param angle The angle being rotated in radians (use right handed rule)
-     */
-    function getRotationMatrix(axis: Vec3, angle: number): Matrix3 {
-        const [x, y, z] = axis;
-        const cos = Math.cos(angle);
-        const mcos = 1 - cos;
-        const sin = Math.sin(angle);
-        return [
-            [cos + x * x * mcos, x * y * mcos - x * sin, x * z * mcos + y * sin],
-            [y * x * mcos + z * sin, cos + y * y * mcos, y * z * mcos - x * sin],
-            [z * x * mcos - y * sin, z * y * mcos + x * sin, cos + z * z * mcos]
-        ];
-    }
+    // There are 6 faces, 0=L,1=R,2=D,3=U,4=B,5=F
+    export type Face = 0 | 1 | 2 | 3 | 4 | 5;
+    // Tracks the face that each original face has ended up on
+    export type FaceMap = [Face, Face, Face, Face, Face, Face] & { 6?: void };
+    // A position vector that tracks x, y, and z values
+    export type Position = Readonly<[number, number, number] & { 3?: void }>;
 
     /**
      * A cubie is an individual cube inside the Rubik's Cube. It is completely represented by it's starting position 
      * and a rotation matrix
      */
-    type Cubie = { readonly startPos: Vec3, readonly rotationMatrix: Matrix3 }
+    export type Cubie = { readonly startPos: Position, readonly faces: FaceMap }
 
     /** 
      * We maintain a list of all the cubies in the cube. These cubies are sorted such that the 0th cube is LDB and 
      * the last cube is RUF and that it first increases in the x direction, then y, then z. We also maintain the size
      * of the cube as described by createCube.
      */
-    export type Cube = { readonly cubies: Cubie[], readonly size: number };
+    export type Cube = { readonly cubies: ReadonlyArray<Cubie>, readonly size: number };
 
     /**
      * Creates a new Cube of a given size
@@ -53,17 +25,52 @@ namespace Rubik {
      */
     export function createCube(size: number): Cube {
         const cubies: Cubie[] = [];
-        for (let z = 0; z < size; z++) {
-            for (let y = 0; y < size; y++) {
-                for (let x = 0; x < size; x++) {
-                    cubies.push({ startPos: [x, y, z], rotationMatrix: IdentityMatrix });
-                }
-            }
-        }
+        range(0, size).forEach(z => {
+            range(0, size).forEach(y => {
+                range(0, size).forEach(x => {
+                    cubies.push({ startPos: [x, y, z], faces: [0, 1, 2, 3, 4, 5] });
+                });
+            });
+        });
         return { cubies, size };
     }
 
-    export type Layer = { readonly axisNumber: number, readonly layerNum: number };
+    // There are three axes, 0=x, 1=y, 2=z
+    export type Axis = 0 | 1 | 2;
+    export type Layer = { readonly axis: Axis, readonly layerNum: number };
+
+    /**
+    * Rotates a given layer of the cube and returns the new cube
+    * @param cube The cube which is getting it's layer rotated
+    * @param rotationLayer The information about the layer being rotated
+    */
+    export function rotateLayer(cube: Cube, rotationLayer: Layer): Cube {
+        const getCubieIndex = (i: number, j: number) => getCubieIndexInLayerPos(cube.size, i, j, rotationLayer);
+        const cubies = cube.cubies.slice(); // Create a copy of the cube
+        range(0, cube.size).forEach(i => {
+            range(0, cube.size).forEach(j => {
+                const oldCubie = cube.cubies[getCubieIndex(cube.size - j - 1, i)];
+                const newFaceMap = getFacesAfterRotation(oldCubie.faces, rotationLayer.axis);
+                cubies[getCubieIndex(i, j)] = { startPos: oldCubie.startPos, faces: newFaceMap };
+            });
+        });
+        return { cubies, size: cube.size }
+    }
+
+    /**
+     * Returns the FaceMap after a rotation on a given axis
+     * @param faces A FaceMap which is being rotated
+     * @param axis The axis of rotation
+     */
+    function getFacesAfterRotation(faces: FaceMap, axis: Axis): FaceMap {
+        let cycle: FaceMap;
+        switch (axis) {
+            case 0: cycle = [0, 1, 4, 5, 2, 3]; break;
+            case 1: cycle = [5, 4, 2, 3, 0, 1]; break;
+            case 2: cycle = [2, 3, 1, 0, 4, 5]; break;
+        }
+        return faces.map(f => faces[cycle[f]]) as FaceMap;
+    }
 
     /**
      * Will return the index into the cubies array belonging to the (i, j)th cubie in the layer
@@ -74,27 +81,10 @@ namespace Rubik {
      */
     function getCubieIndexInLayerPos(cubeSize: number, i: number, j: number, layer: Layer): number {
         const pos = [i, j];
-        pos.splice(layer.axisNumber, 0, layer.layerNum);
+        pos.splice(layer.axis, 0, layer.layerNum);
         return pos[0] + cubeSize * pos[1] + (cubeSize ** 2) * pos[2]
     }
 
-    /**
-     * Rotates a given layer of the cube and returns the new cube
-     * @param cube The cube which is getting it's layer rotated
-     * @param rotationLayer The information about the layer being rotated
-     */
-    export function rotateLayer(cube: Cube, rotationLayer: Layer): Cube {
-        const axis = IdentityMatrix[rotationLayer.axisNumber];
-        const rotationMatrix = getRotationMatrix(axis, Math.PI / 2);
-        const getCubieIndex = (i: number, j: number) => getCubieIndexInLayerPos(cube.size, i, j, rotationLayer);
-        const cubies = cube.cubies.slice(); // Create a copy of the cube
-        for (let i = 0; i < cube.size; i++) {
-            for (let j = 0; j < cube.size; j++) {
-                const oldCubie = cube.cubies[getCubieIndex(cube.size - j - 1, i)];
-                const newCubieRotMatrix = multiplyMatrix(oldCubie.rotationMatrix, rotationMatrix);
-                cubies[getCubieIndex(i, j)] = { startPos: oldCubie.startPos, rotationMatrix: newCubieRotMatrix };
-            }
-        }
-        return { cubies, size: cube.size }
-    }
+    // Helper function for generation the interval [0, max)
+    const range = (max: number) => Array.from({ length: max }, (_, k) => k);
 }
